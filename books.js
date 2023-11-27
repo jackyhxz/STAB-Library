04854fe1794086d83d4c8c9e8ccec84bfcee2614
Jacky Huang <zzandkun@gmail.com>
2023-11-27 10:00:01 -0500

    migrate everything to the firebase

--- books.js
+++ books.js
@@ -19,10 +19,12 @@ const db = getFirestore(app);
 //this takes a file and uploads it to the database
 //to run you need to make an upload file html input with
 //id called URL
+
+//runs the code once and don't call it again until you want to make some changes to the data already in the firebase 
 export async function importToDatabase () {
   const booksInDatabase = await getDocs(collection(db, "library"));
     booksInDatabase.forEach((book) =>{
-        deleteDoc(doc(db, "library", book.id));
+        deleteDoc(doc(db, "library", book.id)); //deleteDoc basically deletes everything in the firebase before uploading again
     }
     )
   // try{
@@ -70,11 +72,139 @@ export async function importToDatabase () {
   document.getElementsByTagName("body").style.cursor = "auto";
 }
 
+// runs the function when refreshing the page: to get the books from firebase
 export const getBookData = async function(){
     const booksInDatabase = await getDocs(collection(db, "library"));
-    booksInDatabase.forEach((books) => {
-        var books = [];
-        books.push(books.data().title, books.data().author, books.data().summary, books.data().genre, books.data().room, books.data().shelf);
-        localStorage.setItem("spread-sheet", JSON.stringify(data));//send the data to local storage
+    var books = [];
+    booksInDatabase.forEach((book) => {
+        books.push(book.data().title, book.data().author, book.data().summary, book.data().genre, book.data().room, book.data().shelf);
     })
+    //localStorage.setItem("book-sheet", book);//send the data to local storage
+    //localStorage.setItem("book-count", booksInDatabase.data().count);
+    //console.log("refresh");
+    display(books);
+}
+
+
+function makeButton(title) {
+    const button = document.createElement('button');
+    button.textContent = "Check out"; 
+    button.addEventListener("click", showCheckOut); //call the function "showCheckOut" when click
+    button.classList.add("check-out-button"); //add class for css use
+    //return button =  `<button onclick="showCheckOut()" class="check-out-button" value='` + title + `'>` + "Check Out" + `</button>`
+    return button;
+}
+// when checking out a book it no longer shows the books and shows the checkout page
+function showCheckOut () { 
+    document.getElementById("book-all").style.display = "none"
+    document.getElementById("check-out-all").style.display = "grid"
+}
+
+// allows us to add an event listener to elements added to the DOM
+$('body').on('click', '.check-out-button', function() {
+    console.log(1)
+    let fired_button = $(this).val();
+    localStorage.setItem("checkOutBook", fired_button);  
+    location.href = "checkOut.html"
+});
+
+// Select all checkboxes with the name 'genre' using querySelectorAll.
+var checkboxes = document.querySelectorAll("input[type=checkbox][name=genre]");
+let enabledSettings = []
+
+export function display(books) {
+    
+    // displays the books, (but why it's slow though, probably need to fix that later?)
+    document.getElementById("div-books").innerHTML = "";
+    for (let i = 0; i < books.length; i += 6) {
+        if (i < books.length - 12) {
+            document.getElementById("div-books").innerHTML = document.getElementById("div-books").innerHTML + "<h3>" + books[i] + "</h3><p>" + books[i+1] + "</p><p>" + books[i+2] + "</p>" + "<p>Check Out Copy In Room: " + books[i+4] + "<br>On Shelf: " + books[i+5] + "</p></br>"; //make button here corresponds to the page of checkout book?
+            document.getElementById("div-books").appendChild(makeButton(books[i]));
+            var hzRule = document.createElement('hr');// make a hr, as you cannot directly add a <hr> in appendChild
+            document.getElementById("div-books").appendChild(hzRule);
+            //console.log(books[i]);
+            //console.log(books[i+5]);
+        } else {
+            document.getElementById("div-books").innerHTML = document.getElementById("div-books").innerHTML + "<h3>" + books[i] + "</h3><p>" + books[i+1] + "</p><p>" + books[i+2] + "</p>" + "<p>Check Out Copy In Room: " + books[i+4] + "<br>On Shelf: " + books[i+5] + "</p></br>";
+            document.getElementById("div-books").appendChild(makeButton(books[i]));
+        }
+    }
+
+    // narrows down the displayed books when the person is typing in the search box
+// goes through all of the books and see if the input matches to the title, description, or author
+// if it the input does match to any of those then it adds that book to an array
+// that array is then passed through a funciton and then displayed on the html
+    document.getElementById("searching-for-books").oninput = () => {
+        let search = document.getElementById("searching-for-books").value;
+        var results = [];
+        for (let i = 0; i < books.length; i += 6) {
+            if (books[i].toLowerCase().includes(search.toLowerCase()) || books[i + 1].toLowerCase().includes(search.toLowerCase()) || books[i + 2].toLowerCase().includes(search.toLowerCase())) {
+                results.push(books[i], books[i + 1], books[i + 2], books[i + 3], books[i + 4], books[i + 5]);
+            }
+        }
+    // updates the page to display books that match the search
+    updateSearching(results);
+    }
+
+/*
+For IE11 support, replace arrow functions with normal functions and
+use a polyfill for Array.forEach:
+https://vanillajstoolkit.com/polyfills/arrayforeach/
+*/
+
+// Use Array.forEach to add an event listener to each checkbox.
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+        enabledSettings =
+            Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+                .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+                .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+
+        //console.log(enabledSettings)
+        limitGenre(enabledSettings, books)
+    })
+});
+}
+
+
+
+// after somemone has searched for a book this will look through the title author and description
+// if it finds a match it adds it to a new array that will then display only the items
+// in that array on the screen
+export function updateSearching(results) {
+    document.getElementById("div-books").innerHTML = "";
+    for (let i = 0; i < results.length; i += 6) {
+        if (i < results.length - 6) {
+            document.getElementById("div-books").innerHTML = document.getElementById("div-books").innerHTML + "<h3>" + results[i] + "</h3><p>" + results[i + 1] + "</p><p>" + results[i + 2] + "<br><br> Check Out Copy In Room: " + results[i + 4] + "<br>On Self: "+ results[i + 5] + "</p><br>";
+            document.getElementById("div-books").appendChild(makeButton(results[i]));
+            var hzRule = document.createElement('hr');// make a hr, as you cannot directly add a <hr> in appendChild
+            document.getElementById("div-books").appendChild(hzRule);
+        } else {
+            document.getElementById("div-books").innerHTML = document.getElementById("div-books").innerHTML + "<h3>" + results[i] + "</h3><p>" + results[i + 1] + "</p><p>" + results[i + 2]  + "<br><br> Check Out Copy in Room: " + results[i + 4] + "<br>On Self: "+ results[i + 5] + "</p><br>";
+            document.getElementById("div-books").appendChild(makeButton(results[i]));
+        }
+    }
+    document.getElementById("div-books").scrollTop = 0;
+}
+
+
+
+// takes the array from when a check box is clicked on and if the array isn't empty
+// then it adds all books that match any of the items in the genre array to a new array
+// then the books in the new array are displayed on the page
+// if the enabledSettings array is empty then all the books are displayed because no checkboxes are checked
+export function limitGenre(enabledSettings, books) {
+    var arr = []
+    if (enabledSettings.length > 0) {
+        for (let i = 0; i < books.length; i += 6) {
+            if (enabledSettings.includes(books[i + 3])) {
+                //console.log(1)
+                arr.push(books[i], books[i + 1], books[i + 2], books[i + 3], books[i + 4], books[i + 5]);
+                //console.log(arr)
+            }
+        }
+        updateSearching(arr)
+        return;
+    }
+    display(books)
 }
