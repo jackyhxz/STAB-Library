6b1ef4f973dc44f2e217434b2ce34bf1cbfcb49a
Elitenick1 <elitenickytkhutsishvili@gmail.com>
2023-12-06 14:40:57 -0500

    creation

---
+++ checkedOutBooks.js
@@ -0,0 +1,6 @@
+// This js will be for CHECKEDOUTBOOKS.HTml
+//make it so when we enter an email it will show the books with your email and marked as Checked-Out
+//if there are no books with your email, then it will return "your email is not associated with any books"
+//if your email is only associated with returned books then it will show the books and say it is returned
+//if your have a book checkedout under your email then it will show the book
+//next to the checkout book there will be a "return" button and maybe other buttons
